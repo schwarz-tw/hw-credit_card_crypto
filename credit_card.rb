e744ab776a2e2a80fbb1654731062ad515e21c9d
schwarz-tw <schwarz.tsai@gmail.com>
2022-03-10 23:26:07 +0800

    finish part a

--- credit_card.rb
+++ credit_card.rb
@@ -1,20 +1,31 @@
-require_relative './luhn_validator.rb'
+# frozen_string_literal: true
+
+require_relative './luhn_validator'
 require 'json'
 
+# Top level doc here :)
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
-  def to_json
+  def to_json(*_args)
     {
       # TODO: setup the hash with all instance vairables to serialize into json
+      'number' => @number,
+      'expiration_date' => @expiration_date,
+      'owner' => owner,
+      'credit_network' => credit_network
     }.to_json
   end
 
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,11 +1,19 @@
+# frozen_string_literal: true
+
+# Top level doc :)
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
+  # rubocop:disable Metrics/AbcSize
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    nums_p = nums_a.reverse.map.with_index { |item, index| index.odd? ? item * 2 : item }
+    sum_digits = nums_p.map { |product| product.to_s.chars.map(&:to_i).sum }
+    ((10 - sum_digits.sum % 10) % 10).zero?
   end
+  # rubocop:enable Metrics/AbcSize
 end
