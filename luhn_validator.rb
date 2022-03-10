f915dbdb0e7298afc5008e6178d94747849b3921
schwarz-tw <schwarz.tsai@gmail.com>
2022-03-10 23:50:02 +0800

    some improvement of part a

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -6,14 +6,16 @@ module LuhnValidator
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-  # rubocop:disable Metrics/AbcSize
+  def multiplier(nums_a)
+    nums_a.reverse.map.with_index { |item, index| index.odd? ? item * 2 : item }
+  end
+
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
-    nums_p = nums_a.reverse.map.with_index { |item, index| index.odd? ? item * 2 : item }
+    nums_p = multiplier(nums_a)
     sum_digits = nums_p.map { |product| product.to_s.chars.map(&:to_i).sum }
     ((10 - sum_digits.sum % 10) % 10).zero?
   end
-  # rubocop:enable Metrics/AbcSize
 end
